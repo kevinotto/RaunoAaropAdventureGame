78875a5ec6276b63c7f51be2d3083b357b5640bd
Ognegaz <73790725+Ognegaz@users.noreply.github.com>
2022-11-19 20:16:18 +0200

    v1

---
+++ New Project/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_1 : NPC {
+	 
+	// Use this for initialization
+	public override void OnSetUpDialogue() {
+		Speech.AddDialogue("0", "Welcome adventurer!", "1");
+		Speech.AddDialogue("1", "Collect coins here", "2");
+		Speech.AddDialogue("2", "Find your powerup", "3");
+		Speech.AddDialogue("3", "Move Across levels to unlock the secret", "4");
+		Speech.AddDialogue("4", "Good Luck!");
+	}
+
+	// Triggered when the player comes in range of the NPC
+	public override void OnTriggerNPC( Collider other ){
+		Speech.SetDialogue("0");
+	}
+}
---
+++ New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int coins;
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start() {
+		coins = 0;
+	}
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {  
+			
+			Destroy(collided.gameObject);
+			if (coins == 0)
+            {
+				HUD.Message("You got a coin!");
+            }
+            else
+            {
+				HUD.Message("You got another coin !");
+            }
+
+			coins++;
+			HUD.UpdateCoinCount(coins);
+		}
+
+        if (collided.gameObject.name == "Powerup Object 1")
+		{
+			Destroy(collided.gameObject);
+			Abilities.doubleJumpEnabled = true;
+			HUD.Message("Double jump activated");
+		}
+
+	}
+}
